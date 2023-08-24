346b19dc4fe2f1bec9161dc0ba0cf25abd5f70d0
Sebastian Salhon <sebastiansalhon@gmail.com>
2023-08-24 13:19:11 -0300

    Se agrego funcion para los botones de contador

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -6,12 +6,25 @@ const ItemCount = () => {
 
 const [contador, setContador] = useState (0)
 
+//Funcion para sumar en el contador hasta maximo 10 unidades
+const sumarContador = () => {
+    if(contador < 10){
+        setContador (contador + 1)
+    }
+}
+
+//Funcion para restar el contador hasta 0
+const restarContador = () => {
+    if (contador > 0){
+        setContador (contador - 1)
+    }
+}
 return (
 <>
     <div className = 'contador'>
-    <Button colorScheme='blue'onClick={() => setContador (contador + 1)}>+</Button>
+    <Button id='botonsuma' colorScheme='blue'onClick={sumarContador}>+</Button>
     <h4 className='numeroContador' >{contador}</h4> 
-    <Button colorScheme='red' onClick={() => setContador (contador - 1)}>-</Button>
+    <Button id='botonresta' colorScheme='red' onClick={restarContador}>-</Button>
     </div>
 </>
 )
